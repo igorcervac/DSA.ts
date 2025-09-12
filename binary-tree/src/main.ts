7cf6f1f0eb6a319e7a4ea5face60d3ea04e4f57f
igorcervac <igor.cervac@outlook.com>
2025-09-12 08:58:09 -0400

    Implement binary tree

---
+++ binary-tree/src/main.ts
@@ -0,0 +1,21 @@
+import BinaryTree from "./binary-tree.ts";
+
+const binaryTree = new BinaryTree<number>();
+
+binaryTree.insert(9);
+binaryTree.insert(7);
+binaryTree.insert(11);
+binaryTree.insert(5);
+binaryTree.insert(8);
+
+console.group("In-order:");
+binaryTree.traverseInOrder();
+console.groupEnd();
+
+console.group("Pre-order:");
+binaryTree.traversePreOrder();
+console.groupEnd();
+
+console.group("Post-order:");
+binaryTree.traversePostOrder();
+console.groupEnd();
